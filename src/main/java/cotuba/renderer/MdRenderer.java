a5d9efc22b68b15f14886773166a85ca53712365
Fernando Alvarez <fernando.alvarez@docket.com.br>
2019-08-15 22:47:10 -0300

    Separação de responsabilidades do MdRenderer em mais métodos

--- src/main/java/cotuba/renderer/MdRenderer.java
+++ src/main/java/cotuba/renderer/MdRenderer.java
@@ -15,57 +15,76 @@ import java.nio.file.Path;
 import java.nio.file.PathMatcher;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.stream.Collectors;
 import java.util.stream.Stream;
 
 public class MdRenderer {
 
+	private static final PathMatcher MATCHER = FileSystems.getDefault().getPathMatcher("glob:**/*.md");
+
 	public List<Capitulo> renderizarMdParaHtml(Path diretorioDosMd) {
 
-		final List<Capitulo> capitulos = new ArrayList<>();
+		final List<Path> arquivosMD = this.listarArquivos(diretorioDosMd);
+		final List<Capitulo> capitulos = new ArrayList<>(arquivosMD.size());
 
-		PathMatcher matcher = FileSystems.getDefault().getPathMatcher("glob:**/*.md");
-		try (Stream<Path> arquivosMD = Files.list(diretorioDosMd)) {
-			arquivosMD.filter(matcher::matches).sorted().forEach(arquivoMD -> {
-				final Capitulo capitulo = new Capitulo();
-				final Parser parser = Parser.builder().build();
-				final Node document;
-				try {
-					document = parser.parseReader(Files.newBufferedReader(arquivoMD));
-					document.accept(new AbstractVisitor() {
-						@Override
-						public void visit(Heading heading) {
-							if (heading.getLevel() == 1) {
-								// capítulo
-								String tituloDoCapitulo = ((Text) heading.getFirstChild()).getLiteral();
-								capitulo.setTitulo(tituloDoCapitulo);
-							} else if (heading.getLevel() == 2) {
-								// seção
-							} else if (heading.getLevel() == 3) {
-								// título
-							}
-						}
-
-					});
-				} catch (Exception ex) {
-					throw new RuntimeException("Erro ao fazer parse do arquivo " + arquivoMD, ex);
-				}
-
-				try {
-					HtmlRenderer renderer = HtmlRenderer.builder().build();
-					capitulo.setConteudoHtml(renderer.render(document));
-
-				} catch (Exception ex) {
-					throw new RuntimeException("Erro ao renderizar para HTML o arquivo " + arquivoMD, ex);
-				}
-
-				capitulos.add(capitulo);
-
-			});
-		} catch (IOException ex) {
-			throw new RuntimeException("Erro tentando encontrar arquivos .md em " + diretorioDosMd.toAbsolutePath(), ex);
+		for (Path arquivoMD : arquivosMD) {
+			capitulos.add(this.gerarCapitulo(arquivoMD));
 		}
 
 		return capitulos;
+	}
+
+	private List<Path> listarArquivos(Path diretorioDosMd) {
+		try (Stream<Path> arquivosMD = Files.list(diretorioDosMd)) {
+			return arquivosMD.filter(MATCHER::matches).sorted().collect(Collectors.toList());
+
+		} catch (IOException e) {
+			throw new RuntimeException("Erro tentando encontrar arquivos .md em " + diretorioDosMd.toAbsolutePath(), e);
+		}
+	}
+
+	private Capitulo gerarCapitulo(Path arquivoMD) {
+
+		final Capitulo capitulo = new Capitulo();
+		final Node document = this.parsearDocumento(arquivoMD, capitulo);
+
+		try {
+			final HtmlRenderer renderer = HtmlRenderer.builder().build();
+			capitulo.setConteudoHtml(renderer.render(document));
+
+		} catch (Exception ex) {
+			throw new RuntimeException("Erro ao renderizar para HTML o arquivo " + arquivoMD, ex);
+		}
+
+		return capitulo;
+	}
+
+	private Node parsearDocumento(Path arquivoMD, Capitulo capitulo) {
+		final Parser parser = Parser.builder().build();
+
+		try {
+			final Node document = parser.parseReader(Files.newBufferedReader(arquivoMD));
+
+			document.accept(new AbstractVisitor() {
+				@Override
+				public void visit(Heading heading) {
+					if (heading.getLevel() == 1) {
+						// capítulo
+						final String tituloDoCapitulo = ((Text) heading.getFirstChild()).getLiteral();
+						capitulo.setTitulo(tituloDoCapitulo);
+					} else if (heading.getLevel() == 2) {
+						// seção
+					} else if (heading.getLevel() == 3) {
+						// título
+					}
+				}
+			});
+
+			return document;
+
+		} catch (Exception ex) {
+			throw new RuntimeException("Erro ao fazer parse do arquivo " + arquivoMD, ex);
+		}
 	}
 
 }
