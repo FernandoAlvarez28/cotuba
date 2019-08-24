0f00d88f7a4c9767a3e13e96f00c24505887b573
Fernando Alvarez <fernando44ca@gmail.com>
2019-08-24 11:26:09 -0300

    Utilização de loader para a interface para tema

--- src/main/java/cotuba/tema/AplicadorTema.java
+++ src/main/java/cotuba/tema/AplicadorTema.java
@@ -1,24 +1,25 @@
 package cotuba.tema;
 
 import cotuba.domain.Capitulo;
+import cotuba.plugin.TemaLoader;
 import org.apache.commons.lang3.StringUtils;
 import org.jsoup.Jsoup;
 import org.jsoup.nodes.Document;
 
-public class AplicadorTema {
+import java.util.List;
 
-	private static final String CSS = StringUtils.join(
-			"h1 {",
-			"   border-bottom: 1px dashed black;",
-			" }"
-	);
+public class AplicadorTema {
 
 	public void aplicarTema(Capitulo capitulo) {
 		final String html = capitulo.getConteudoHtml();
 
 		final Document document = Jsoup.parse(html);
 
-		document.select("head").append(StringUtils.join("<style>", CSS, "</style>"));
+		final List<String> temas = TemaLoader.getTemas();
+
+		for (String tema : temas) {
+			document.select("head").append(StringUtils.join("<style>", tema, "</style>"));
+		}
 
 		capitulo.setConteudoHtml(document.html());
 	}
